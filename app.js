0aff8a40bbffc9028e88edd80cfa0a44b6c91d75
Omar <omar@omarsalempro.com>
2020-02-13 17:01:12 -0700

    Add onReadyStateChange ...

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 const ajax = new XMLHttpRequest();
 
+ajax.onreadystatechange = function() {
+  console.log('state changed: ', ajax.readyState, ajax.status);
+  if (ajax.readyState === 4 && ajax.status === 200){
+    console.log(ajax.responseText);
+  }
+}
 ajax.open('GET', 'https://randomuser.me/api', true);
 
 ajax.send()
